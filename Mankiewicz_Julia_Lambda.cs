48b549c9a7b41df51f6f3037355c26dda95eba9d
mankiewicx <116458917+mankiewicx@users.noreply.github.com>
2023-03-19 19:15:21 +0100

    Add files via upload

---
+++ Mankiewicz_Julia_Lambda.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            Expression<Func<int>> sum = () => 1 + a + 3 + 4;
+            Func<int> wynik = sum.Compile();
+            Console.WriteLine(wynik());
+        }
+    }
+}
